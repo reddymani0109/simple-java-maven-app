615b68c1ced279b5fb1142823146bc8f75129488
Manikanta Reddy <155880965+reddymani0109@users.noreply.github.com>
2024-05-18 15:51:00 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Manikanta!";
+    private static final String MESSAGE = "Hello Manikanta Reddy!";
 
     public App() {}
 
